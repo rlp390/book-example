cebe22944ed52d27a3e6b6ee302402f3ade6b51e
rlp390 <rlp390@hotmail.com>
2022-01-07 19:47:13 -0600

    testing

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,8 +5,8 @@ from fabric.api import cd, env, local, run
 REPO_URL = 'https://github.com/rlp390/book-example.git'
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    run(f'mkdir -p {site_folder}')
+    site_folder = '/home/' + env.user + '/sites/' + env.host
+    run('mkdir -p ' + site_folder)
     with cd(site_folder):
         _get_latest_source()
         _update_virtualenv()
@@ -18,24 +18,24 @@ def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
-        run(f'git clone {REPO_URL} .')
+        run('git clone ' + REPO_URL + ' .')
     current_commit = local("git log -n 1 --format=%H", capture=True)
-    run(f'git reset --hard {current_commit}')
+    run('git reset --hard ' + current_commit)
 
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
-        run(f'python3 -m venv virtualenv')
+        run('python -m venv virtualenv')
     run('./virtualenv/bin/pip install -r requirements.txt')
 
 def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME={env.host}')
+    append('.env', 'SITENAME=' + env.host)
     current_contents = run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        append('.env', 'DJANGO_SECRET_KEY=' + new_secret)
 
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
